7f28a3448f9ce9674e83a06b1905aead2f78aebc
epihovegor <epihove@gmail.com>
2023-09-25 16:29:58 +0300

    Lesson_1

---
+++ Lesson_1/main.py
@@ -0,0 +1,15 @@
+# number = 10
+# # Печатает 10
+# print(number)
+# # Выводит класс number
+# print(type(number))
+
+# # Просто печатает имя Bob
+# name = 'Bob'
+# print(name)
+# print(type(name))
+# print(id(name))
+
+name = input('Как вас зовут?\n')
+print('Привет, ' + name)
+print(f'Привет, {name}')
